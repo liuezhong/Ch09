cd121d1d82b4c0d75867aa62326eddd14492bc88
zhongliue <liuezhong@163.com>
2017-09-03 21:42:14 +0800

    树回归

--- mytry.py
+++ mytry.py
@@ -8,14 +8,20 @@ import numpy as np
 
 # myDat = regTrees.loadDataSet('ex00.txt')
 # myDat = regTrees.loadDataSet('ex0.txt')
-myDat = regTrees.loadDataSet('ex2.txt')
-# # print(myDat)
-myMat = np.mat(myDat)
+
+# print(myDat)
+# myMat = np.mat(myDat)
 # print(regTrees.createTree(myMat))
 
 #后剪枝
-myTree = regTrees.createTree(myMat, ops=(0,1))
-myDatTest = regTrees.loadDataSet('ex2test.txt')
-myMat2Test = np.mat(myDatTest)
-regTrees.prune(myTree,myMat2Test)
+# myDat = regTrees.loadDataSet('ex2.txt')
+# myMat = np.mat(myDat)
+# myTree = regTrees.createTree(myMat, ops=(0,1))
+# myDatTest = regTrees.loadDataSet('ex2test.txt')
+# myMat2Test = np.mat(myDatTest)
+# print(regTrees.prune(myTree,myMat2Test))
 
+#模型树
+myDat = regTrees.loadDataSet('exp2.txt')
+myMat = np.mat(myDat)
+print(regTrees.createTree(myMat,regTrees.modelLeaf,regTrees.modelErr,(1,10)))
